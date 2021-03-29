46bbc4697652835efe29685bdb60cebc327a9dbb
James Tripp <james.tripp@warwick.ac.uk>
2021-03-29 16:16:06 +0100

    Updated data documentation

--- R/data.R
+++ R/data.R
@@ -1,6 +1,15 @@
 #' Example stanfit object
 #'
-#' An example stanfit object.
+#' An example stanfit object following the example fit
+#'  of a heirarchical schools model from the
+#'  pystan documentation.
+#'  'https://pystan.readthedocs.io/en/latest/'.
+#'
 #' @format A stanfit object with 18 parameters,
 #'  3 chains, 10000 iterations and 500 warmup samples
+#' @examples
+#' # show model used to create sample
+#' sample_stanfit@stanmodel
+#' # print model summary
+#' sample_stanfit
 "sample_stanfit"
